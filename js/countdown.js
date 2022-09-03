de7f2394745bda7945668491a3048815034e1db7
Gabriel Seffrin <gabrielfseffrin@gmail.com>
2022-09-02 23:14:34 -0300

    timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,91 +1,129 @@
-let minutesUser
-let breakUser
-let longBreak
+let timer = document.getElementById('minutes-user').value * 60
+let id
 
-let timePomodoro
+function temporizador(time) {
+    timer = time
+    var display = document.querySelector('#time')
 
-let refreshIntervalId 
+    id = setInterval(function () {
+        var minutes = parseInt(timer / 60)
+        var seconds = parseInt(timer % 60)
 
-function onclickStartButton() {
-    clearInterval(refreshIntervalId)
-    setValueTimer()
-    var duration = minutesUser * 60
-    display = document.querySelector('#time')
-    startPomodoro(duration, display)
+        //caso o valor seja menor que 10, add 0 esquerda
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds
+
+        display.textContent = `${minutes}:${seconds}`
+        if (--timer < 0) {
+            clearInterval(id)
+        }
+    }, 1000)
 }
 
-document.getElementById('stop').onclick = () => {
-    clearInterval(refreshIntervalId)
-
+function start() {
+    temporizador(timer)
 }
-function setValueTimer() {
 
-    clearInterval(refreshIntervalId)
+function stops() {
+    clearInterval(id)
+}
+
+function resume() {
+    temporizador(timer)
+}
+
+console.log(timer)
+
+// let minutesUser
+// let _break
+// let longBreak
+
+// let timePomodoro
+// let _tempoRestante
+
+// let refreshIntervalId 
+
+// function onclickStartButton() {
+//     clearInterval(refreshIntervalId)
+//     setValueTimer()
+//     var duration = minutesUser * 60
+//     display = document.querySelector('#time')
+//     startPomodoro(duration, display)
+// }
+
+
+// document.getElementById("enviar").onclick = function () {
+//     setValueTimer();
+// }
+
+// document.getElementById('stop').onclick = () => {
+//     clearInterval(refreshIntervalId)
+// }
+// function setValueTimer() {
+
+//     clearInterval(refreshIntervalId)
     
-    minutesUser = document.getElementById('minutes-user').value 
-    breakUser = document.getElementById('break').value 
-    longBreak = document.getElementById('long-break').value
+//     minutesUser = document.getElementById('minutes-user').value 
+//     _break = document.getElementById('break').value 
+//     longBreak = document.getElementById('long-break').value
 
-    if (minutesUser < 0)
-        minutesUser *= -1
-    if (breakUser < 0)
-        breakUser *= -1
-    if (longBreak < 0)
-        longBreak *= -1
-}
+//     if (minutesUser < 0)
+//         minutesUser *= -1
+//     if (_break < 0)
+//         _break *= -1
+//     if (longBreak < 0)
+//         longBreak *= -1
+// }
 
-document.getElementById("enviar").onclick = function (e) {
-    setValueTimer();
-    e.preventDefault();
-}
+// function startPomodoro(duration, display) {  
+//     var _tempoRestante = duration,
+//         minutes, seconds
 
-function startPomodoro(duration, display) {  
-    var timer = duration,
-        minutes, seconds
+//     refreshIntervalId = setInterval(function () {
+//         minutes = parseInt(_tempoRestante / 60, 10);
+//         seconds = parseInt(_tempoRestante % 60, 10);
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//         display.textContent = minutes + ":" + seconds;
+//         _tempoRestante
 
-    refreshIntervalId = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            clearInterval(refreshIntervalId)
-            shortBreak((breakUser * 60), display)
-        }
-    }, 1000);
-}
+//         if (--_tempoRestante < 0) {
+//             clearInterval(refreshIntervalId)
+//             shortBreak((_break * 60), display)
+//         }
+//     }, 1000);
+// }
 
-let shortBreak = (breakUser, display) => {
-    var timer = breakUser,
-        minutes, seconds
+// let shortBreak = (_break, display) => {
+//     var timer = _break,
+//         minutes, seconds
 
-    refreshIntervalId = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            clearInterval(refreshIntervalId)
-            longBreakTimer((longBreak * 60), display)
+//     refreshIntervalId = setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//         display.textContent = minutes + ":" + seconds;
+//         if (--timer < 0) {
+//             clearInterval(refreshIntervalId)
+//             longBreakTimer((longBreak * 60), display)
             
-        }
-    }, 1000);
-}
+//         }
+//     }, 1000);
+// }
 
-let longBreakTimer = (longBreak, display) => {
-    var timer = longBreak,
-        minutes, seconds
+// let longBreakTimer = (longBreak, display) => {
+//     var timer = longBreak,
+//         minutes, seconds
 
-    refreshIntervalId = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            clearInterval(refreshIntervalId)
-        }
-    }, 1000);
-}
+//     refreshIntervalId = setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+//         display.textContent = minutes + ":" + seconds;
+//         if (--timer < 0) {
+//             clearInterval(refreshIntervalId)
+//         }
+//     }, 1000);
+// }
--- js/teste.js
+++ js/teste.js
@@ -1,31 +1,38 @@
 export default class Countdown {
 
-    constructor(minutes, breakTime, longBreakTime, display) {
+    constructor(minutes, breakTime, longBreakTime) {
         this._minutes = minutes * 60
         this._breakTime = breakTime * 60
         this._longBreakTime = longBreakTime * 60
         this.refreshIntervalId = null
-        this.display = display
+        this.display = document.querySelector('#time')
     }
 
-    startPomodoro() {
-        
-        var timer = (this._minutes * 60),
-            minutes, seconds
+    temporizador(time, display) {
 
-            this.refreshIntervalId = setInterval(function() {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            display.textContent = minutes + ":" + seconds;
-            if (--timer < 0) {
-                clearInterval(refreshIntervalId)
+        timer = time, minutes, seconds
+    
+        var id = setInterval(function() {
+    
+            this.minutes = parseInt(timer / 60)
+            seconds = parseInt(timer % 60)
+    
+            //caso o valor seja menor que 10, add 0 esquerda
+            minutes = minutes < 10 ? "0" + minutes : minutes
+            seconds = seconds < 10 ? "0" + seconds : seconds
+    
+            display.textContent = `${minutes} : ${seconds}`
+    
+            if(--timer < 0) {
+                clearInterval(id)
             }
         }, 1000)
     }
+    shotBreak() {
+        timer
+    }
 
     get _tempoRestante() { 
-        return 
+        return this.timer
     }
 }
