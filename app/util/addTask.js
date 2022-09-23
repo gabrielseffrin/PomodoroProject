e0fb4d24fe94f7e38b7fbbfcfc8bbd46f880df43
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-09-23 18:54:40 -0300

    clone

--- app/util/addTask.js
+++ app/util/addTask.js
@@ -1,33 +1,46 @@
-import {
-  Task
-} from "../model/task.js"
+//import Task from "./task";
 
-//index of task
-let index
-const tasks = new Array
-let taskString = ``
-
-(function () {
-  index = 0
-}())
-
-console.log(index)
-
-const buttonAddTask = document.querySelector("#text-task")
+const buttonAddTask = document.querySelector("#add-task")
 buttonAddTask.addEventListener("click", addTask)
 
 function addTask() {
-  let taskContent = document.getElementsByName('tasls')[0].value
-  let task = new Task(taskContent, true)
-
-  taskString += `<div class="task">
-  <input type="checkbox" name="" id="">
-  ${tasks[0]}
-      </div> `
-
-  tasks.push(task)
-  index++
-  document.getElementById('square-task').innerHTML = taskString
-
-
+    alert('bund')
+    document.getElementById('footer').innerHTML = modal
+    //     `<div class="form-check">
+    //     <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    //     <label class="form-check-label" for="flexCheckDefault">
+    //       Default checkbox
+    //     </label>
+    //   </div>`
 }
+
+const element = document.querySelector("#add-task");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+    alert(123)
+}
+
+const modal = `<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>`
--- app/util/task.js
+++ app/util/task.js
@@ -1,4 +1,4 @@
-export class Task {
+export default class Task {
     constructor(text, isVisible) {
         this.text = text
         this.isVisible = isVisible
