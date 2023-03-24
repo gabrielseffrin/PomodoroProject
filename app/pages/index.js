32f358661d53c492452f48e325d8c22620811d5f
gabrielseffrin <gabrielfseffrin@gmail.com>
2023-03-24 19:29:06 -0300

    web3

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,95 +1,22 @@
 'use strict';
 
-import {
-    User
-} from '../model/user.js';
 
-import {
-    Task
-} from '../model/task.js';
+let login = document.getElementById('logar').onclick = () => {
 
-import {
-    USER_NAME,
-    USER_TASKS
-} from '../shared.js';
+    let email = document.getElementById('inputEmail').value + '';
 
-import {
-    timeToFocus,
-    updateTimeUser
-} from '../util/countdown.js';
+    if (email === 'fff')
+        window.location.href = '/app/pages/perfil/perfil.html';
+    else
+        console.log(email);
 
-let tasks = [];
-
-function isPossibleToAddTask() {
-    return tasks.length < 6 ? true : false;
-}
-
-let writeTask = (taskString) => {
-    document.getElementById('square-task').innerHTML = taskString;
 };
 
-function addTask(inputTaks) {
-
-    if (isPossibleToAddTask()) {
-
-
-        console.log(inputTaks)
-
-        let task = JSON.parse(localStorage.getItem(USER_TASKS));
-        let taskString = '';
-        console.log(task)
-
-        task.forEach(task => {
-            tasks.push(new Task(task._text, task._isVisible));
-        });
-
-        tasks.forEach(task => {
-            taskString += task.getTask();
-        });
-        writeTask(taskString);
-        console.log(tasks);
-        localStorage.setItem(USER_TASKS, JSON.stringify(tasks));
-    } else {
-        window.alert('Número máximo de task!');
-    }
-
-}
-
-document.getElementById('submit-task').onclick = () => {
-    if (isPossibleToAddTask()) {
-        addTask(tasks.push(new Task(document.getElementsByName('task-content')[0].value, true)));
-
-    }
-};
-
-function initializeUsers() {
-    if (localStorage.getItem(USER_NAME) != null && localStorage.getItem(USER_TASKS) != null) {
-        addTask();
-        updateTimeUser(0);
-        return;
-    }
-    let user = new User(USER_NAME, 0);
-    let userTasks = [];
-
-    localStorage.setItem(USER_NAME, JSON.stringify(user));
-    localStorage.setItem(USER_TASKS, JSON.stringify(userTasks));
-}
-
+/*
 function isNameValid() {
-    let name = document.getElementById('inputName').value;
+   let name = document.getElementById('inputName').value;
 
     if (!name.match('[A-Z][a-z].* [A-Z][a-z].*'))
         window.alert('Nome Inválido');
 }
-
-document.getElementById('salvar').onclick = () => {
-    isNameValid();
-};
-
-// salvano uma tarefa 
-const buttonAddTask = document.querySelector('#submit-task');
-buttonAddTask.addEventListener('click', addTask);
-
-window.onload = function () {
-    initializeUsers();
-};
+*/
--- app/shared.js
+++ app/shared.js
@@ -1,14 +1,6 @@
 'use strict';
 
-const USER_NAME = 'user.padrao';
-const USER_TASKS = 'user.tasks';
-
 $(function () {
     $('#footer').load('/footer.html');
     $('#header').load('/header.html');
 });
-
-export {
-    USER_NAME,
-    USER_TASKS
-};
